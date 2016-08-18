a1073605b5ac288f62fea4f05a4e730898440f04
Paulo Teixeira <paulo.teixeira@outlook.com>
2016-08-17 21:54:46 -0300

    fix ssl cert creation

--- scripts/install_moodle.sh
+++ scripts/install_moodle.sh
@@ -92,9 +92,9 @@
 
     #enable ssl 
     a2enmod rewrite ssl
-    echo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /moodle/certs/apache.key -out /moodle/certs/apache.crt -subj "/C=BR/ST=SP/L=Sao Paulo/O=IT/CN=$siteFQDN"
+    echo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /moodle/certs/apache.key -out /moodle/certs/apache.crt -subj "/C=BR/ST=SP/L=SaoPaulo/O=IT/CN=$siteFQDN"
 
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /moodle/certs/apache.key -out /moodle/certs/apache.crt -subj "/C=BR/ST=SP/L=Sao Paulo/O=IT/CN=$siteFQDN"
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /moodle/certs/apache.key -out /moodle/certs/apache.crt -subj "/C=BR/ST=SP/L=SaoPaulo/O=IT/CN=$siteFQDN"
 
     #update virtual site configuration 
     echo -e '
