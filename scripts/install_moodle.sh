7c7d28fc36572ee2db79564b7055ac2a51319815
Paulo Teixeira <paulo.teixeira@outlook.com>
2017-04-14 19:08:06 -0300

    updates

--- scripts/install_moodle.sh
+++ scripts/install_moodle.sh
@@ -83,8 +83,8 @@
     sudo apt-get -y update > /dev/null
     sudo apt-get install -y --force-yes mariadb-client 
     sudo apt-get install -y --force-yes graphviz aspell php5.6-common php5.6-soap php5.6-json php5.6-zip         > /tmp/apt6.log
-    sudo apt-get install -y --force-yes php5.6-bcmath php5.6-gd php5.6-mysql php5.6-xmlrpc php5.6-intl php5.6-xml php5.6-bz2 php5.6-redis >> /tmp/apt6.log
-    sudo apt-get install -y --force-yes php5.6-curl php5.6-mysql >> /tmp/apt6.log
+    sudo apt-get install -y --force-yes php5.6-bcmath php5.6-gd php5.6-mysql php5.6-xmlrpc php5.6-intl php5.6-xml php5.6-bz2 >> /tmp/apt6.log
+    sudo apt-get install -y --force-yes php5.6-redis php5.6-curl php5.6-mysql >> /tmp/apt6.log
 
     # install Moodle 
     echo '#!/bin/bash
