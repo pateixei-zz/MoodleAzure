08944ed4ecb09deb77a3affce6b09b7475a7ad74
Paulo Teixeira <paulo.teixeira@outlook.com>
2017-04-14 11:42:22 -0300

    updates

--- scripts/setup_moodle.sh
+++ scripts/setup_moodle.sh
@@ -52,8 +52,10 @@ sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082
 sudo add-apt-repository 'deb http://mirror.edatel.net.co/mariadb/repo/10.1/ubuntu trusty main'
 sudo apt-get -y update > /dev/null
 
-sudo apt-get install -y graphviz aspell php5.6-common php5.6-soap php5.6-json php5.6-zip php5.6-bcmath php5.6-gd php5.6-mysql php5.6-xmlrpc php5.6-intl php5.6-xml php5.6-bz2 php5.6-redis php5.6-curl        >> /tmp/apt6.log
-sudo apt-get install -y mariadb-client php5.6-mysql
+sudo apt-get install -y mariadb-client
+sudo apt-get install -y graphviz aspell php5.6-common php5.6-soap php5.6-json php5.6-zip php5.6-bcmath \ 
+                        php5.6-gd php5.6-mysql php5.6-xmlrpc php5.6-intl php5.6-xml php5.6-bz2 \
+                        php5.6-redis php5.6-curl php5.6-mysql       >> /tmp/apt6.log
 
 # create gluster mount point
 sudo mkdir -p /moodle
